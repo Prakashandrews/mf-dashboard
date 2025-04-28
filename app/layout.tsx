2d936282ca5933d46e626a6e6a5faad8d608f7e3
Prakash M.S <prakash.subramanian@yara.com>
2025-04-29 00:57:54 +0800

    title update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'V-Dashboard',
+  title: 'MF-Dashboard',
   description: 'A modern dashboard',
 };
 
